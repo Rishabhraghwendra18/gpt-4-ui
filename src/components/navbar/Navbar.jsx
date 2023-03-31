16c00bd8dc2f1b54623d74847b20c33c2e6f53a0
rishabhraghwendra@18 <rishabhraghwendra2002@gmail.com>
2023-03-31 19:32:42 +0530

    renamed gpt3 in navbar to gpt4

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
         </div>
         <div className="gpt3__navbar-links_container">
           <p><a href="#home">Home</a></p>
-          <p><a href="#wgpt3">What is GPT3?</a></p>
+          <p><a href="#wgpt3">What is GPT4?</a></p>
           <p><a href="#possibility">Open AI</a></p>
           <p><a href="#features">Case Studies</a></p>
           <p><a href="#blog">Library</a></p>
